86da42e95f9e398dbb716f770aaaa893b30ea177
matiara <49988089+matiara@users.noreply.github.com>
2020-06-14 19:40:34 -0300

    14/06 creacion metodos json

--- src/json/ManejadorJson.java
+++ src/json/ManejadorJson.java
@@ -1,5 +1,58 @@
 package json;
 
-public class ManejadorJson {
+import com.company.Usuario;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+public class ManejadorJson {
+    private JSONObject data;
+
+    public ManejadorJson() {
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+            FileReader reader = new FileReader("src/json/data.json");
+
+            try {
+                Object obj = jsonParser.parse(reader);
+                this.data = (JSONObject)obj;
+                System.out.println("Informacion en el sistema: " + this.data);
+            } catch (Throwable var6) {
+                try {
+                    reader.close();
+                } catch (Throwable var5) {
+                    var6.addSuppressed(var5);
+                }
+
+                throw var6;
+            }
+
+            reader.close();
+        } catch (FileNotFoundException var7) {
+            var7.printStackTrace();
+        } catch (IOException var8) {
+            var8.printStackTrace();
+        } catch (ParseException var9) {
+            var9.printStackTrace();
+        }
+
+    }
+
+    private static void parseEmployeeObject(JSONObject employee) {
+        JSONObject employeeObject = (JSONObject)employee.get("personas");
+        String firstName = (String)employeeObject.get("nombre");
+        System.out.println(firstName);
+        String lastName = (String)employeeObject.get("apellido");
+        System.out.println(lastName);
+        float website = (Float)employeeObject.get("edad");
+        System.out.println(website);
+    }
+
+   
 }
