1189916deb5e8359ecfa835e7197a1b10430669d
matiara <49988089+matiara@users.noreply.github.com>
2020-06-18 01:14:16 -0300

    18/06 se agregan metodos json a hashmap

--- src/com/company/Empresa.java
+++ src/com/company/Empresa.java
@@ -1,36 +1,34 @@
 package com.company;
 
-import com.company.TipoAvion;
+import com.company.Avion;
 import com.company.Usuario;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import reserva.Lugares;
 import reserva.Reserva;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class Empresa {
     private String nombre;
     private HashMap<Integer, Usuario> mapUsuarios;
-    private ArrayList<TipoAvion> listaAviones;
+    private HashMap<Integer, Avion> mapAviones;
     private HashMap<Integer, Reserva> mapReservas;
-    ///ingrese el numero de la persona que realizo la reserva
 
     /// en el map reservas se tiene que poder pasar el dni de la persona que reservó, y los valores de la reserva.
 
     public Empresa(String nombre){
         this.nombre = nombre;
         this.mapUsuarios = new HashMap<Integer,Usuario>();
-        this.listaAviones = new ArrayList<TipoAvion>();
+        this.mapAviones = new HashMap<Integer, Avion>();
         this.mapReservas = new HashMap<Integer, Reserva>();
     }
     public void agregarUsuario(Usuario u){
         mapUsuarios.put(u.getDni(),u);
     }
-    //agregar avion
-    //agregarreserva
+    public void agregarAvionn(Avion t){ mapAviones.put(t.getTipoAvion(),t); }
+    public void agregarReserva(Reserva r){ mapReservas.put(r.getDni(),r); }
 
 
     public void mostrarUsuarios(){
@@ -54,12 +52,54 @@ public class Empresa {
 
         for (int i = 0; i < allAviones.size(); i++) {
 
+            JSONObject avion= (JSONObject) allAviones.get(i);
+            Avion t = new Avion(Float.parseFloat(avion.get("capConbustible").toString()),Float.parseFloat(avion.get("Costokm").toString()),Integer.parseInt(avion.get("maxCapacity").toString()),Float.parseFloat(avion.get("velMaxima").toString()),Motor.valueOf(avion.get("motor").toString()),Integer.parseInt(avion.get("tipoAvion").toString()));
+            agregarAvionn(t);
 
+        }
+    }
+
+    public void inicializarReservas(JSONArray allReservas){
+
+        for (int i = 0; i < allReservas.size(); i++) {
+
+            JSONObject reserva = (JSONObject) allReservas.get(i);
+            Reserva r = new Reserva(Integer.parseInt(reserva.get("fecha").toString()),Lugares.valueOf(reserva.get("origen").toString()),Lugares.valueOf(reserva.get("destino").toString()),Integer.parseInt(reserva.get("canAcompañantes").toString()),Integer.parseInt(reserva.get("selecAvion").toString()),Float.parseFloat(reserva.get("costoTotal").toString()),Integer.parseInt(reserva.get("dni").toString()));
+            agregarReserva(r);
 
         }
     }
 
 
+
+
+
+    ///funcion para how to convert from jsonarray to arraylist(aviones)
+    ///funcion para how to convert from jsonarray to hashmap(reserva)
+
+
+    /*
+    public void agregarAvion(Avion avion){
+        listaAviones.add(avion);
+    }
+
+    public void mostrarArregloAvion(){
+        int cont=0;
+        for(int i=0;i<Avion.size();i++){
+            System.out.println("Nº" +cont + Avion.get(i).toString());
+            cont++;
+        }
+    }*/
+
+    public void generarReserva(Usuario usuario, Reserva reserva){
+        mapReservas.put(usuario.getDni(),reserva);
+    }
+
+    public void mostrarReservas(){
+        mapReservas.get(mapUsuarios.keySet());
+    }
+
+}
 
 
 
