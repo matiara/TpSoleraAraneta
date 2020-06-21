f0589413f0fa670d2db6679b835a0199daac3e06
matiara <49988089+matiara@users.noreply.github.com>
2020-06-21 19:09:14 -0300

    21/06 se agrega tarifa y dni

--- src/clasesextra/Bronze.java
+++ src/clasesextra/Bronze.java
@@ -4,8 +4,8 @@ import com.company.Avion;
 import com.company.Motor;
 
 public class Bronze extends Avion {
-    public Bronze(float capConbustible, float costoXkm, int sizePasajeros, float velMaxima, Motor motor,String tipoAvion) {
-        super(capConbustible, costoXkm, sizePasajeros, velMaxima, motor, tipoAvion);
+    public Bronze(float capConbustible, float costoXkm, int sizePasajeros, float velMaxima, Motor motor,String tipoAvion,int id, int tarifa) {
+        super(capConbustible, costoXkm, sizePasajeros, velMaxima, motor, tipoAvion,id,tarifa);
     }
 
 }
