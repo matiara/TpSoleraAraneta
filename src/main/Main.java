856e48310e6825883116790047ae90f40b2bcebd
matiara <49988089+matiara@users.noreply.github.com>
2020-06-22 12:56:05 -0300

    22/06 Errores de tipeado incorrecto

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,13 +17,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
+    Scanner scanner = new Scanner(System.in);
+    Empresa empresa = new Empresa("Aerotaxi Club"); /// CREACION DE EMPRESA
+    ManejadorJson manejador = new ManejadorJson(empresa);
 
-        /// cambiar la key en el map de reservas
-
-        Scanner scanner = new Scanner(System.in);
-        Empresa empresa = new Empresa("Aerotaxi Club"); /// CREACION DE EMPRESA
-        ManejadorJson manejador = new ManejadorJson(empresa);
-
+    
         /// CREANDO MENU INTERACTIVO
         int opcion=0;
 
@@ -42,108 +41,95 @@ public class Main {
                 switch(opcion){
                     case 1:
 
-                        Usuario nuevoUsuario = new Usuario();
-                        nuevoUsuario.crearUsuario();
-                        empresa.agregarUsuario(nuevoUsuario);
-                        manejador.writeUser(nuevoUsuario);
-                        salir = true;
-                        break;
+                    /// PROBANDO DE CREAR UN USUARIO EN DONDE SE AGREGE A EMPRESA, Y SE LO MUESTRE.
+
+                    Usuario nuevoUsuario = new Usuario();
+                    nuevoUsuario.crearUsuario();
+                    empresa.agregarUsuario(nuevoUsuario);
+                    manejador.writeUser(nuevoUsuario);
+                    salir = true;
+                    break;
 
                     case 2:
 
-                        ///CREANDO RESERVA              /// ------------------------------------------> corregir esto
-                        Reserva reserva = new Reserva();
+                    ///CREANDO RESERVA
+                    Reserva reserva = new Reserva();
 
-                        /// CORROBORAR DNI
 
-                        int encontrado = 0;
-                        encontrado = empresa.buscarDni();
+                    ///FECHA
+                    reserva.indicarFecha();
 
-                        if(encontrado == 0){
-                            System.out.println("No puedes realizar reserva por que el DNI no se encontro");
-                            break;
+                    /// ORIGEN - DESTINO
 
+                    int origen = reserva.selecOrigen();
+                    int destino = reserva.selecDestino();
+
+                    if(origen == destino){
+                        System.out.println("El destino y el origen no pueden ser iguales");
+                    }
+
+                    /// CANTIDAD ACOMPAÑANTES.
+                    reserva.putCantAcompañantes();
+                    System.out.println("La cantidad de acompañantes es: " + reserva.getCantAcompañantes());
+
+                    /// SELECCIONAR AVION.
+                    empresa.mostrarAviones();
+                    reserva.elegirAvion();
+
+                    /// OBTENIENDO DISTANCIA.
+                    System.out.println("Obteniendo distancia");
+                    int distancia = reserva.obtenerDistancia();
+                    System.out.println("" + distancia);
+
+                    ///MOSTRANDO LOS DATOS DE LA RESERVA
+                    int confirmar=0;
+                    int check=0;
+
+                    while (check == 0){
+                        System.out.println("Los datos de la reserva son:");
+                        reserva.calcularCosto(empresa.getListaAviones());
+                        reserva.mostrarValores();
+
+                        System.out.println("¿Confirmar Reserva?");
+                        System.out.println("1 - SI");
+                        System.out.println("2 - NO");
+                        confirmar = scanner.nextInt();
+
+                        if (confirmar == 1){
+                            empresa.agregarReserva(reserva);
+                            manejador.writeReserva(reserva);
+                            System.out.println("Su reserva a sido cargada");
+                            check = 1;
                         }
-                        else{
-                            System.out.println("Su DNI ha sido encontrado, porfavor ingrese los valores requeridos");
-                        }
-
-
-                        ///FECHA
-                        reserva.indicarFecha();
-
-                        /// ORIGEN - DESTINO
-
-                                    int origen = reserva.selecOrigen();
-                                    int destino = reserva.selecDestino();
-
-                                    if(origen == destino){
-                                        System.out.println("El destino y el origen no pueden ser iguales");
-                                    }
-
-                                    /// CANTIDAD ACOMPAÑANTES.
-                                    reserva.putCantAcompañantes();
-                                    System.out.println("La cantidad de acompañantes es: " + reserva.getCantAcompañantes());
-
-                                    /// SELECCIONAR AVION.
-                                    empresa.mostrarAviones();
-                                    reserva.elegirAvion();
-
-                                    /// OBTENIENDO DISTANCIA.
-                                    int distancia = reserva.obtenerDistancia();
-
-                                    ///MOSTRANDO LOS DATOS DE LA RESERVA
-
-
-                        /// implementar funcion en vez de resolver el codigo aca.
-
-                        int confirmar=0;
-                        int check=0;
-
-                        while (check == 0){
-                            System.out.println("Los datos de la reserva son:");
-                            reserva.getCostoTotal();
-                            System.out.println("\n");
-                            reserva.mostrarValores();
-
-                            System.out.println("¿Confirmar Reserva?");
-                            System.out.println("1 - SI");
-                            System.out.println("2 - NO");
-                            confirmar = scanner.nextInt();
-
-                            if (confirmar == 1){
-                                empresa.agregarReserva(reserva);
-                                manejador.writeReserva(reserva);
-                                System.out.println("Su reserva a sido cargada");
-                                check = 1;
-                            }
-                            if(confirmar == 2){
-                                System.out.println("Acaba de cancelar la reserva");
-                                check = 1;
-                            }
-
-                            if (confirmar <1 || confirmar > 2){
-                                check = 0;
-                                System.out.println("Valor incorrecto, vuelva a ingresar el valor");
-                            }
+                        if(confirmar == 2){
+                            System.out.println("Acaba de cancelar la reserva");
+                            check = 1;
                         }
 
-                        salir = true;
-                        break;
+                        if (confirmar <1 || confirmar > 2){
+                            check = 0;
+                            System.out.println("Valor incorrecto, vuelva a ingresar el valor");
+                        }
+                    }
+
+                    salir = true;
+                    break;
 
                     case 3:
 
-                        /// MOSTRANDO USUARIOS
-                        System.out.println("Mostrando usuarios: ");
-                        empresa.mostrarUsuarios();
+                    /// MOSTRANDO USUARIOS
+                    System.out.println("Mostrando usuarios: \n");
+                    empresa.mostrarUsuarios();
+                    /// MOSTRANDO AVIONES
+                    System.out.println("Mostrando aviones");
+                    empresa.mostrarAviones();
 
 
-                        ///ACA TIENE QUE MOSTRAR TODAS LAS RESERVAS HECHAS HASTA EL MOMENTO Y SELECCIONAR CON UN INT CUAL HAY QUE CANCELAR Y QUE ESA LA BORRE.
                     break;
 
-                        default:
-                            System.out.println("Las opciones son 1,2 o 3");
-                            }
+                    default:
+                        System.out.println("Las opciones son 1,2 o 3");
+                        }
 
                 }catch(InputMismatchException ex) {
                     System.err.println("Debes introducir un numero");
